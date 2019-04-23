f3ab8b9ba823436a1d6ec6d79987d6247e72d9f4
julia <julia27354@gmail.com>
2019-04-23 11:55:12 -0300

    criar listas

---
+++ aula01.py
@@ -0,0 +1,133 @@
+ #peso_lbs=input("descubra quanto vale em kg")
+#peso_kg = int(peso_lbs) * 0.45
+
+#print(peso_kg)
+
+
+#name = "Ana"
+#color = input("qual é sua cor favorita")
+
+#print("A"+name+"gosta de"+color)
+
+#name = "Ana Beatriz"
+
+#print(name[1:5])
+#print(len(name))
+#print(name.find("i"))
+#print(name.replace("Beatriz", "Morita"))
+
+#condição
+
+#summer = False
+#winter = False
+
+#if summer:
+  #  print("use óculos de sol")
+  #  print("use filtro")
+  #  print("vá a praia")
+#elif winter:
+   # print("não esquece o casaco filho")
+#else:
+  #  print("não use óculos, durma.")
+
+#green_bank = True
+#criminal = False
+#money= True
+
+#if green_bank and money and not criminal:
+   # print = ("ih deu ruim!")
+
+#while
+ #i=0
+# while i < 5:
+    # print(i)
+    # i = i + 1
+#print("acabou")
+
+#secret_number = 2
+#i = 0
+
+#while i < 3:
+ #   chute = int(input("Qual é o número secreto"))
+  #  i = i + 1
+ #if chute == secret_number:   
+  #  print("Parabéns! vc acertou")      
+   # break
+#else:
+ #   print("vc errou!")
+    
+
+
+
+#iphone  = 8000
+#Xiaomi = 5000
+#positivo = 450
+
+#val = int(input ("quanto dinheiro voce tem")
+
+#if  ==
+ #     print("voce pode comprar")
+#else      
+
+#listas
+#names = ["Ana","Raiane","Ju","Samuel"]
+#print(names[1])
+
+#numbers = [1,4,6,6,9,345,101]
+#nova_lista = []
+
+#for number in numbers:
+ #   if number not in nova_lista:
+  #      nova_lista.append(number)
+#print(nova_lista)        
+
+#lista2 = numbers.copy
+#lista.remove(6)
+#print(lista2)
+#print(numbers)
+#numbers.append(20)
+#numbers.insert(0,99)
+#numbers.remove(345)
+#numbers.clear()
+#numbers.pop() 
+#print(numbers)   
+#lista2 = numbers.copy
+#numbers.append(222)
+#print(lista2)
+
+
+#tupla ou tuple
+#cursos = ("Matematica", "Ingles","Biologia")
+
+#dicionarios
+
+#pessoa = {
+  #  "name": "hayane",
+   # "age":22,
+    #"address": "área de preservaçãp florestal",
+    #"skills": "python","c"
+#}
+
+#print(pessoa["name"])
+#print(pessoa["address"])
+
+
+
+
+lista = [1,2,3,8,45,12]
+max = listas[0]
+
+for lista in listas:
+    if lista > max:]
+        max = lista
+print(max)
+# ou
+
+print(max(lista))
+
+media_nota = int (input("qual é a sua Média"))
+falta = 2
+if media_nota >= 6 and falta =< 2:
+    print("voce foi a provado")
+else:
+    print("voce foi reprovado")    
